9124570feeb538d64ed2f9b6822cb3e8e2b5d7ea
Abid Ali <abdsoftfsd@gmail.com>
2018-12-28 20:16:54 +0500

    Restaurant Progressive Web App Final Build

---
+++ precache-manifest.2e0c267454634e392e072f7f0886dc53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d759bbfb8cacc018a32676ca53314f4",
+    "url": "/static/media/description.9d759bbf.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4ad393c15702000dad05",
+    "url": "/static/js/main.4ad393c1.chunk.js"
+  },
+  {
+    "revision": "8d24a8f93709cc3cee50",
+    "url": "/static/js/1.8d24a8f9.chunk.js"
+  },
+  {
+    "revision": "4ad393c15702000dad05",
+    "url": "/static/css/main.46b47ff8.chunk.css"
+  },
+  {
+    "revision": "42cac3129a3432c00b6f13dc89ca84ed",
+    "url": "/index.html"
+  }
+];
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Restaurant App
+A progressive web app for restaurant menu display.  
+
+[Live Demo](https://abid-restaurant-demo.firebaseapp.com/)
